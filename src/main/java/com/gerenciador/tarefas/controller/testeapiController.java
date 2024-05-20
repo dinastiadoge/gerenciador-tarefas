907b81eb3f2099a2d2d48d1ab3d2b9f71d4a6b48
Lucas Lopes <lucasslopes2004@gmail.com>
2024-05-20 15:00:13 -0300

    Add rota de teste

--- src/main/java/com/gerenciador/tarefas/controller/testeapiController.java
+++ src/main/java/com/gerenciador/tarefas/controller/testeapiController.java
@@ -1,6 +1,7 @@
 package com.gerenciador.tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class testeapiController {
     private String teste(){
         return "Sucesso";
     }
+    @GetMapping("/bem-vindo")
+    public String testeBemvindo(@RequestParam(name = "nome") String nome){
+        return "Olá " + nome + ", seja bem vindo";
+    }
+
 
 }
